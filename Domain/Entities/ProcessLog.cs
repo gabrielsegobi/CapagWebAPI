f32ff26c71699fcbbcf110c944f6ef73441f85bd
gabriel segobi <gabriel.souza@gmaster.com.br>
2026-02-19 10:07:47 -0300

    Outras Alterações

--- Domain/Entities/ProcessLog.cs
+++ Domain/Entities/ProcessLog.cs
@@ -1,12 +1,27 @@
 ﻿namespace Domain.Entities
 {
-    public class ProcessLog: ITenantEntity
+    public class ProcessLog : ITenantEntity
     {
+        public ProcessLog(long idEmpresa, string acao, string mensagem)
+        {
+            IdEmpresa = idEmpresa;
+            Acao = acao;
+            CreatedAt = GetDateTimeNow();
+            Mensagem = mensagem;
+        }
+
         public long Id { get; set; }
         public long IdEmpresa { get; set; }
         public string Acao { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public string Mensagem { get; set; } = string.Empty;
         public long IdTenant { get; set; }
+
+        private DateTime GetDateTimeNow()
+        {
+            var now = DateTime.UtcNow;
+            return now.AddHours(-3);
+        }
+
     }
 }
