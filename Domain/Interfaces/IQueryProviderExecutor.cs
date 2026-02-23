748cb95683a91af022f955f0349de8672c0cb293
gabriel segobi <gabriel.souza@gmaster.com.br>
2026-02-23 10:13:41 -0300

    ajustes

---
+++ Domain/Interfaces/IQueryProviderExecutor.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Interfaces
+{
+    public interface IQueryProviderExecutor<TEntity>
+    {
+        Task<int> CountAsync();
+        Task<List<TEntity>> ToListAsync(int skip, int take);
+    }
+}
