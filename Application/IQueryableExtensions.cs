7c753a8562e824be97455ed493b881b457b3a148
gabriel segobi <gabriel.souza@gmaster.com.br>
2026-02-23 10:13:12 -0300

    ajuste

--- Application/IQueryableExtensions.cs
+++ Application/IQueryableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Filters;
 using Domain.Contracts.Responses;
+using Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application
@@ -37,4 +38,43 @@ namespace Application
             return new PagedApiResponse<TDestination>(pageData, dtos);
         }
     }
+
+    //public static class IQueryableExtensions
+    //{
+    //    public static async Task<PagedApiResponse<TDestination>> ReadPage<TEntity, TDestination>(
+    //        this IQueryProviderExecutor<TEntity> executor,
+    //        BaseFilter filter,
+    //        Func<IQueryable<TEntity>, IQueryable<TEntity>>? applyFilters = null,
+    //        Func<IEnumerable<TEntity>, IEnumerable<TDestination>>? mapFunc = null,
+    //        IQueryable<TEntity>? query = null) // opcional, caso precise aplicar filtros
+    //        where TEntity : class
+    //    {
+    //        if (query != null && applyFilters != null)
+    //        {
+    //            query = applyFilters(query);
+    //        }
+
+    //        // total
+    //        var total = await (query != null ? query.CountAsync() : executor.CountAsync());
+
+    //        int skip = ((filter.Page ?? 1) - 1) * (filter.PageSize ?? 10);
+    //        int take = filter.PageSize ?? 10;
+
+    //        // dados
+    //        var data = await (query != null ? query.Skip(skip).Take(take).ToListAsync() : executor.ToListAsync(skip, take));
+
+    //        // mapeamento
+    //        var dtos = mapFunc != null ? mapFunc(data) : data.Cast<TDestination>();
+
+    //        var pageData = new PageData
+    //        {
+    //            Page = filter.Page ?? 1,
+    //            PageSize = filter.PageSize ?? 10,
+    //            Total = total
+    //        };
+
+    //        return new PagedApiResponse<TDestination>(pageData, dtos);
+    //    }
+    //}
 }
+ 
